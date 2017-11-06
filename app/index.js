d8d931a7af43b566e2aec44c58874ddb4e41a6d8
Alex pandian <email2alexpandian@gmail.com>
2017-11-06 18:00:48 +0530

    06/11/2017 6:00 pm:

--- app/index.js
+++ app/index.js
@@ -1,8 +1,29 @@
-var login = angular.module("login", ["apiHandler"]);
+var login = angular.module("login", ["apiHandler", "ui.router"]);
+
+login.config(function($stateProvider, $urlRouterProvider){
+	$stateProvider.state({
+		name : "login",
+		url  : "/login",
+		controller : "loginController",
+		templateUrl : "./app/templates/login.html"
+	});
+	$stateProvider.state({
+		name : "join",
+		url  : "/join",
+		templateUrl : "./app/templates/join.html"
+	});
+	$stateProvider.state({
+		name : "forgetPassword",
+		url  : "/forgetPassword",
+		templateUrl : "./app/templates/forgetPassword.html"
+	});
+	$urlRouterProvider.otherwise('/login');
+});
 
 login.constant("appOriginUrl", "http://localhost");
 login.constant("apiRootUrl", "http://localhost");
 login.constant("_ak_", "login");
+
 login.service("loginrequestService", ["requestService", "_ak_","apiRootUrl", "$http", function(requestService, apiKey, apiRootUrl, $http){
 
 	this.checkLogin = function(requestData){
@@ -10,8 +31,8 @@ login.service("loginrequestService", ["requestService", "_ak_","apiRootUrl", "$h
 	};
 
 }]);
-login.controller("testController", ["loginrequestService", "$scope", function(loginrequestService, $scope){
-	$scope.testApiCall = function(){
+login.controller("loginController", ["loginrequestService", "$scope", function(loginrequestService, $scope){
+	$scope.validateLogin = function(loginFrom){ console.log(loginFrom);
 		loginrequestService.checkLogin({ name : "alex" });
 	};
 	
--- application/views/login.php
+++ application/views/login.php
@@ -10,22 +10,64 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<script type="text/javascript" src="./jquery/jquery.min.js"></script>
 </head>
 <body ng-app="login" >
-	<header>
-		<div class="container-fluid" >
-			<h2>Digital Diary</h2>
-		</div>
-	</header>
-	<div class="col-md-12 col-sm-12" >
-		<div class="col-md-6 col-sm-12" >
-			
-		</div>
+
+	<div class="container">
+	    <div class="row">
+	        <h2>Login</h2>
+	    </div>
 	</div>
-	<div ng-controller="testController" >
-		<button ng-click="testApiCall()" >test</button>
-	</div>
+	<br>
+	<br>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-4 col-md-offset-4">
+				<div class="form-body">
+				    <ul class="nav nav-tabs final-login">
+				        <li ui-sref="login" ui-sref-active="active" class="pointer">
+				        	<a>Sign In</a>
+				        </li>
+				        <li ui-sref="join" ui-sref-active="active" class="pointer">
+				        	<a>Join Now!</a>
+				        </li>
+				    </ul>
+				    <div class="tab-content">
+				        <div id="sectionA" class="tab-pane fade in active">
+				        	<div class="innter-form">
+					            <ui-view></ui-view>
+				            </div>
+				            <div class="social-login">
+					            <p>- - - - - - - - - - - - - Sign In With - - - - - - - - - - - - - </p>
+					            <ul>
+						            <li><a href=""><i class="fa fa-facebook"></i> Facebook</a></li>
+						            <li><a href=""><i class="fa fa-google-plus"></i> Google+</a></li>
+						            <li><a href=""><i class="fa fa-twitter"></i> Twitter</a></li>
+					            </ul>
+				            </div>
+				            <div class="clearfix"></div>
+				        </div>
+				        <div id="sectionB" class="tab-pane fade">
+				            <div class="innter-form">
+					            
+				            </div>
+				            <div class="social-login">
+					            <p>- - - - - - - - - - - - - Register With - - - - - - - - - - - - - </p>
+					            <ul>
+						            <li><a href=""><i class="fa fa-facebook"></i> Facebook</a></li>
+						            <li><a href=""><i class="fa fa-google-plus"></i> Google+</a></li>
+						            <li><a href=""><i class="fa fa-twitter"></i> Twitter</a></li>
+					            </ul>
+				            </div>
+				        </div>
+				    </div>
+		    	</div>
+		    </div>
+	    </div>
+    </div>
+
 </body>
 <script type="text/javascript" src="./app/common/api-handler.js"></script>
 <script type="text/javascript" src="./app/index.js"></script>
 <link rel="stylesheet" type="text/css" href="./bootstrap/css/bootstrap.min.css">
 <script type="text/javascript" src="./bootstrap/js/bootstrap.min.js" ></script>
+<link rel="stylesheet" type="text/css" href="./css/style.css">
 </html>
